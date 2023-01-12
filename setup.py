39d76fbe2f88272a7a07be953296fb0e8ced826a
Petr Olah <donthave@email.cz>
2023-01-12 16:14:22 +0100

    Update project url.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='skip-django-generic-m2m-field',
-    version='0.0.9.1',
+    version='0.0.9.2',
     description="Django generic many to many field library.",
     keywords='django, fields, generic',
     author='Lubos Matl',
     author_email='matllubos@gmail.com',
-    url='https://github.com/druids/django-generic-m2m-field',
+    url='https://github.com/skip-pay/django-generic-m2m-field',
     license='MIT',
     package_dir={'generic_m2m_field': 'generic_m2m_field'},
     include_package_data=True,
