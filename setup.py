ba7cf72e2e7a53da41a581e0e827b0a53fd20da0
Petr Olah <donthave@email.cz>
2023-01-12 16:41:31 +0100

    Update germanium.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skip-django-generic-m2m-field',
-    version='0.0.9.2',
+    version='0.0.9.3',
     description="Django generic many to many field library.",
     keywords='django, fields, generic',
     author='Lubos Matl',
@@ -28,7 +28,7 @@ setup(
     ],
     install_requires=[
         'django>=2.2.9, <4.0',
-        'skip-django-chamber>=0.6.16.2'
+        'skip-django-chamber>=0.6.16.3'
     ],
     zip_safe=False
 )
